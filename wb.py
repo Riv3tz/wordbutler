059477af0257224a9bb21038e69cb888c3834f61
Ruchir Harbhajanka <36511798+Riv3tz@users.noreply.github.com>
2019-09-07 20:31:25 +0530

    Added source

---
+++ wb.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+u = "https://api.datamuse.com"
+par = "/words?rel_rhy="
+
+f = input("Enter a word")
+
+f = u + par + f 
+
+r = requests.get(url = f)
+
+data = r.json()
+
+def disp():
+    words = [i['word'] for i in data]
+    print(type(words))
+    print(', '.join(words))
+
+disp()
